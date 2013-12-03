e987532910d9a1babad93d68f19ec89b7a24e63f
Neil Chintomby <nchintomby@gmail.com>
2013-12-03 09:15:02 -0800

    use logging; add timethis decorator to measure function runtime

--- README.md
+++ README.md
@@ -126,3 +126,28 @@ BFS is O(|V| + |E|).
     user    0m0.338s
     sys     0m0.020s
 
+
+####Optimizations:
+
+* baseline (non-threaded)
+  
+  A large amount of time is spent creating the word graph.  This is easiest to see with large words.
+
+        $ python word_ladder.py --debug DEBUG vivification minification
+        [2013-12-03 12:28:09,349][INFO][word_ladder.py:101] Reading dictionary ...
+        [2013-12-03 12:28:09,460][DEBUG][word_ladder.py:29] [timing] create word_set: 0.1107s
+        [2013-12-03 12:28:09,460][INFO][word_ladder.py:114] Grouping 20447 words ...
+        [2013-12-03 12:28:10,159][DEBUG][word_ladder.py:29] [timing] grouping words: 0.6988s
+        [2013-12-03 12:28:10,159][INFO][word_ladder.py:127] Building word graph ...
+        [2013-12-03 12:28:10,375][DEBUG][word_ladder.py:29] [timing] create word graph: 0.2160s
+        [2013-12-03 12:28:10,496][DEBUG][word_ladder.py:27] [timing] __init__: 1.1470s
+        [2013-12-03 12:28:10,496][INFO][word_ladder.py:142] Finding word transformations in word graph (vertices=20447, edges=1804) ...
+        [2013-12-03 12:28:10,496][DEBUG][word_ladder.py:27] [timing] bfs: 0.0000s
+        [2013-12-03 12:28:10,496][DEBUG][word_ladder.py:27] [timing] find_transformation: 0.0002s
+        Found path from `vivification` to `minification`:
+          1: vivification
+          2: vinification
+          3: minification
+        [2013-12-03 12:28:10,498][DEBUG][word_ladder.py:27] [timing] main: 1.1504s
+
+  It takes `0.6988`s to create the word graph, about 60% of the runtime.
--- word_ladder.py
+++ word_ladder.py
@@ -7,10 +7,35 @@
         Optimizations (deque operations are faster, but more memory intensive)
 """
 import argparse
-import sys
 import itertools
+import functools
+import time
+import logging
 
 from collections import deque
+from contextlib import contextmanager
+
+
+def timethis(what=None):
+
+    @contextmanager
+    def benchmark():
+        start = time.time()
+        yield
+        end = time.time()
+        try:
+            logging.debug('[timing] {}: {:0.04f}s'.format(what.__name__, end-start))
+        except AttributeError:
+            logging.debug('[timing] {}: {:0.04f}s'.format(what, end-start))
+
+    if hasattr(what, "__call__"):
+        @functools.wraps(what)
+        def timed(*args, **kwargs):
+            with benchmark():
+                return what(*args, **kwargs)
+        return timed
+    else:
+        return benchmark()
 
 
 class Graph(object):
@@ -34,7 +59,7 @@ class Graph(object):
             edges = self._graph[v2] = []
         edges.append(v1)
 
-
+    @timethis
     def bfs(self, start, end):
         queue = deque([[start]])
         seen = {start}
@@ -67,49 +92,53 @@ class WordGraph(Graph):
         by a `letter distance` of 1.
     """
 
+    @timethis
     def __init__(self, dictionary='/usr/share/dict/words', word_size=5):
         # could serialize graph to reduce startup time for large dictionaries
         super(WordGraph, self).__init__()
         self.edge_count = 0
         self.word_set = set()
-        print >> sys.stderr, 'Reading dictionary ...'
-        try:
-            with open(dictionary, 'r') as f:
-                for word in f:
-                    word = word.strip()
-                    if len(word) != word_size:
-                        continue
-                    self.word_set.add(word.lower())
-        except Exception, e:
-            print >> sys.stderr, 'Failed to load dictionary: `{}` ({})'.format(dictionary, e)
-            return
+        logging.info('Reading dictionary ...')
+        with timethis('create word_set'):
+            try:
+                with open(dictionary, 'r') as f:
+                    for word in f:
+                        word = word.strip()
+                        if len(word) != word_size:
+                            continue
+                        self.word_set.add(word.lower())
+            except Exception, e:
+                logging.info('Failed to load dictionary: `{}` ({})'.format(dictionary, e))
+                return
 
-        print >> sys.stderr, 'Grouping {} words ...'.format(len(self.word_set))
-        words_by_part = {}
-        for w in list(self.word_set):
-            for i in range(word_size):
-                part = '{}?{}'.format(w[:i], w[i+1:])
-                try:
-                    words = words_by_part[part]
-                except KeyError:
-                    words = words_by_part[part] = []
-                words.append(w)
+        logging.info('Grouping {} words ...'.format(len(self.word_set)))
+        with timethis('grouping words'):
+            words_by_part = {}
+            for w in list(self.word_set):
+                for i in range(word_size):
+                    part = '{}?{}'.format(w[:i], w[i+1:])
+                    try:
+                        words = words_by_part[part]
+                    except KeyError:
+                        words = words_by_part[part] = []
+                    words.append(w)
 
-        print >> sys.stderr, 'Building word graph ...'
-        for part,words in words_by_part.iteritems():
-            for v1,v2 in itertools.combinations(words, 2):
-                self.add_edge(v1, v2)
+        with timethis('create word graph'):
+            logging.info('Building word graph ...')
+            for part,words in words_by_part.iteritems():
+                for v1,v2 in itertools.combinations(words, 2):
+                    self.add_edge(v1, v2)
 
-
+    @timethis
     def find_transformation(self, start, end):
         if start not in self.word_set:
-            print >> sys.stderr, '`{}` not in dictionary'.format(start)
+            print '`{}` not in dictionary'.format(start)
             return []
         if end not in self.word_set:
-            print >> sys.stderr, '`{}` not in dictionary'.format(end)
+            print '`{}` not in dictionary'.format(end)
             return []
 
-        print >> sys.stderr, ('Finding word transformations in word graph '
+        logging.info('Finding word transformations in word graph '
                 '(vertices={}, edges={}) ...'.format(len(self.word_set), self.edge_count))
         return self.bfs(start, end)
 
@@ -121,15 +150,20 @@ def test():
             "brand", "braid", "brain"]
 
 
+@timethis
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', default='INFO')
     parser.add_argument('start')
     parser.add_argument('end')
     args = parser.parse_args()
+    logging.basicConfig(
+            format='[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)s] %(message)s',
+            level=getattr(logging, args.debug))
 
     if len(args.start) != len(args.end):
-        print 'start word `{}` is not the same length as end word `{}`'.format(
-            args.start, args.end)
+        print 'start word `{}`({}) is not the same length as end word `{}`({})'.format(
+            args.start, len(args.start), args.end, len(args.end))
         return
 
     wg = WordGraph(word_size=len(args.start))
